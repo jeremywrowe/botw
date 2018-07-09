30d1c319383a074da68a642cb1b78fa2f74bb968
Jeremy W. Rowe <jeremywrowe@users.noreply.github.com>
2018-07-09 16:48:45 -0400

    create initial shell of importer script

---
+++ importer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace importer
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            System.Console.WriteLine("hello there.");
+            return 0;
+        }
+    }
+}
